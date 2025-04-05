6c9dbcda9dd3d3aae8ec390d461c48b2c503886b
gerardo-lopez-fpuna <mikaleale1994@fpuna.edu.py>
2025-04-05 15:56:43 -0300

    Changed

---
+++ src/main/java/com/library/bootstrap/LibraryHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.library.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryHexagonalApplication.class, args);
+	}
+
+}
